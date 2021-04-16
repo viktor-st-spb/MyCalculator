9b55f1eebbfb3e93d97a515f9ecb3c30f4d83475
viktor-st-spb <viktor.st.spb@gmail.com>
2021-04-16 14:29:29 +0300

    Added "+", "-" and "/" functionality

--- app/src/main/java/org/example/mycalculator/MainActivity.kt
+++ app/src/main/java/org/example/mycalculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,7 +34,6 @@ class MainActivity : AppCompatActivity() {
         lastDot = false
     }
 
-
     /**** Decimal point button onClickListener ****/
     fun onDecimalPoint(view: View) {
         //Check whether the last pressed button was numeric
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /**** Check whether pressed button is operator one ****/
+    /**** Check whether pressed button is operator button ****/
     private fun isOperatorAdded(value: String) : Boolean {
         //Check whether the string starts with 'minus'
         return if (value.startsWith("-")){
@@ -62,6 +62,101 @@ class MainActivity : AppCompatActivity() {
             //check whether the string contains / or * or + or - sign
             value.contains("/") || value.contains("*") ||
                     value.contains("+") || value.contains("-")
+        }
+    }
+
+    /**** Equal (=) button onClickListener ****/
+    fun onEqual(view: View){
+        //Check if the entered value is numeric (not an operator)
+        if(lastNumeric){
+            //Transfer the entered data (tvInput) to String
+            var tvValue = tvInput.text.toString()
+
+            var prefix = ""
+
+            try{
+
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                /**** Check whether pressed button is "-" button ****/
+                if(tvValue.contains("-")){
+                //Split thr string into two parts:
+                    //to the left of "-" and
+                    //to the right of "-"
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0]    //left part
+                    var two = splitValue[1]    //right part
+
+                    if (!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                }
+
+                /**** Check whether pressed button is "+" button ****/
+                if(tvValue.contains("+")){
+                    //Split thr string into two parts:
+                    //to the left of "+" and
+                    //to the right of "+"
+                    val splitValue = tvValue.split("+")
+
+                    var one = splitValue[0]    //left part
+                    var two = splitValue[1]    //right part
+
+                    if (!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = (one.toDouble() + two.toDouble()).toString()
+                }
+
+                /**** Check whether pressed button is "*" button ****/
+                if(tvValue.contains("*")){
+                    //Split thr string into two parts:
+                    //to the left of "*" and
+                    //to the right of "*"
+                    val splitValue = tvValue.split("*")
+
+                    var one = splitValue[0]    //left part
+                    var two = splitValue[1]    //right part
+
+                    if (!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput.text = (one.toDouble() * two.toDouble()).toString()
+                }
+
+                /**** Check whether pressed button is "/" button ****/
+                if(tvValue.contains("/")){
+                    //Split thr string into two parts:
+                    //to the left of "/" and
+                    //to the right of "/"
+                    val splitValue = tvValue.split("/")
+
+                    var one = splitValue[0]    //left part
+                    var two = splitValue[1]    //right part
+
+                    if (!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+
+                    //Division by zero
+                    if (two.toDouble() == 0.0){
+                        tvInput.text = "INFINITY"
+                    } else {
+                        tvInput.text = (one.toDouble() / two.toDouble()).toString()
+                    }
+                }
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+
         }
     }
 }
