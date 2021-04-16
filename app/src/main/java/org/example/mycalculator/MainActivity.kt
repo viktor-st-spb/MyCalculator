35d9e3deb4af2555610a94e17c4255381fabfd6c
viktor-st-spb <viktor.st.spb@gmail.com>
2021-04-16 09:36:41 +0300

    Added Decimal Point Button functionality

--- app/src/main/java/org/example/mycalculator/MainActivity.kt
+++ app/src/main/java/org/example/mycalculator/MainActivity.kt
@@ -7,6 +7,13 @@ import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+       /******** Helper Variables ********/
+    //Whether the last presses button is numeric
+    var lastNumeric : Boolean = false
+    //Whether the last presses button is decimal point
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,11 +21,22 @@ class MainActivity : AppCompatActivity() {
 
     fun onDigit(view: View) {
         tvInput.append((view as Button).text)
+        lastNumeric = true
     }
 
     fun onClear (view: View){
         tvInput.text = ""
+        //Restore initial state of helper variables
+        lastNumeric = false
+        lastDot = false
     }
 
-
+    fun onDecimalPoint(view: View) {
+        //Check whether the last pressed button was numeric
+        if (lastNumeric && !lastDot) {
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
 }
